bda9f860be11b3b08e032407e805891ff5264be0
Snoopyh <ismaelxd500@gmail.com>
2023-08-04 19:11:06 -0300

    Last changes

---
+++ src/pages/api/jsx.jsx
@@ -0,0 +1,14 @@
+export default function Jsx() {
+  const a = 4;
+  const b = 3;
+  console.log(a * b);
+  return (
+    <div>
+      <h1>JSX é um conceito Central</h1>
+      <h2>{"testando codigo".toUpperCase()}</h2>
+      <p>
+          {{}}
+      </p>
+    </div>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,9 @@
+import Jsx from "./api/jsx";
+
 export default function Initial() {
   return (
     <div>
+      <Jsx/>
       <h1>Fundamentos de Next.js</h1>
       <h2>go to study this framework</h2>
     </div>
